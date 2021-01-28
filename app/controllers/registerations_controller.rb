c395f90ef41eacfd19b1814915a88edf12cfe4fd
Mahmoud Ali Ibrahim <ma7moud.saraya@gmail.com>
2021-01-28 11:41:58 +0200

    add user registeration / login

---
+++ app/controllers/registerations_controller.rb
@@ -0,0 +1,21 @@
+class RegisterationsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: 'You have been logged in successfully'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  get "about", to: "about#index"
+  get 'about', to: 'about#index'
 
-  root to: "main#index"
+  get 'sign_up', to: 'registerations#new'
+  post 'sign_up', to: 'registerations#create'
+
+  get 'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create'
+
+  delete 'logout', to: 'sessions#destroy'
+
+  root to: 'main#index'
 end
